9d875daacfe95b6ed67567318d1b851023c11433
USER <USER@DESKTOP-MHT6M56>
2021-01-13 10:24:02 +0900

    방명록 리스트

---
+++ src/bbs/VisitInsert.java
@@ -0,0 +1,77 @@
+package bbs;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
+@WebServlet("/VisitInsert")
+public class VisitInsert extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		request.setCharacterEncoding("utf-8");
+		
+		// 클라이언트가 write.html로 요청으로 전송한 값을 읽어옴
+		String writer=request.getParameter("writer");
+		String memo=request.getParameter("memo");
+		
+		System.out.println("작성자 :"+writer);
+		System.out.println("내   용 :"+memo);
+		
+		// 가져온 파라미터 값을 데이터베이스에 저장
+		StringBuffer sql = new StringBuffer();
+		sql.append("insert into visit(no, writer, memo, regdate) ");
+		sql.append("values(visit_seq.nextval, ?,?,sysdate)");
+		
+		// 데이터 베이스 연결
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+/*			
+			// 드라이버 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			// 드라이버 매니저 (url, id, pw)
+			con = DriverManager.getConnection(
+					"jdbc:oracle:thin:@localhost:1521:orcl",
+			        "scott", "tiger");
+	*/		
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			//드라이버 매니저(url, id, pw)
+			con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl"
+					,"scott",
+					"tiger");
+
+			pstmt = con.prepareStatement(sql.toString());
+			pstmt.setString(1, writer);
+			pstmt.setString(2, memo);
+			pstmt.executeUpdate();
+			
+		}catch(SQLException se) {
+			se.printStackTrace();
+		}catch(ClassNotFoundException ce) {
+			ce.printStackTrace();
+		}finally {
+			try {if(pstmt != null) pstmt.close();}catch(SQLException ss) {}
+			try {if(con != null) con.close();}catch(SQLException ss) {}
+		}
+		
+		
+		// 강제 페이지 이동 sendRedirect(이동할 페이지)
+		response.sendRedirect("VisitList");
+		
+		
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+}
---
+++ src/bbs/VisitList.java
@@ -0,0 +1,108 @@
+package bbs;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+import java.sql.*;
+import java.util.*;
+
+
+@WebServlet("/VisitList")
+public class VisitList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void processRequest(HttpServletRequest request,
+	HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out =response.getWriter();
+		
+		try {
+			out.println("<html>");
+			out.println("<head><title>방명록 리스트</title></head>");
+			out.println("<body>");
+			
+			//쿼리 작성
+			StringBuffer sql = new StringBuffer();
+			sql.append("select no, writer, memo, regdate ");
+			sql.append("from visit ");
+			sql.append("order by no desc ");
+			
+			// 연결객체생성
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			
+			try {
+				// 디비연결
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				// 드라이버 매니저 (url, id, pw)
+				con = DriverManager.getConnection(
+						"jdbc:oracle:thin:@localhost:1521:orcl",
+				        "scott", "tiger");
+							
+				pstmt = con.prepareStatement(sql.toString());
+				
+				rs = pstmt.executeQuery();
+				
+				while(rs.next()) {
+				
+					int no = rs.getInt("no");
+					String writer = rs.getString("writer");
+					String memo = rs.getString("memo");
+					java.sql.Date regdate = rs.getDate("regdate");
+					
+					out.println("<table align=center width=500 border=1>");
+					
+					out.println("<tr>");
+					out.println("<th width=50>번호</th>");
+					out.println("<td width=50 align=center>"+no+"</td>");
+				
+					out.println("<th width=70>작성자</th>");
+					out.println("<td width=180 align=center>"+writer+"</td>");
+				
+					out.println("<th width=50>날짜</th>");
+					out.println("<td width=100 align=center>"+regdate+"</td>");
+					
+					out.println("</tr>");
+					
+					out.println("<tr>");
+					out.println("<th width=50>내용</th>");
+					out.println("<td colspan=5>&nbsp;<textarea rows=3 cols=50>"
+					+memo+"</textarea></td>");
+					out.println("</tr>");
+					out.println("</table>");
+					out.println("<p>");
+				}
+				
+			}catch(SQLException ss ) {
+				ss.printStackTrace();
+			}catch(ClassNotFoundException fe) {
+				fe.printStackTrace();
+			}finally {
+				try {if(rs != null) rs.close();}catch(SQLException s) {}
+				try {if(pstmt != null) pstmt.close();}catch(SQLException s) {}
+				try {if(con != null) con.close();}catch(SQLException s) {}
+			}
+			
+			out.println("<p align=center><a href=/JSPExample/bbs/write.html>글쓰기</a></p>");
+			out.println("</body>");
+			out.println("</html>");
+			
+			
+		}finally {
+			out.close();
+		}
+	}
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+}
