4fad10421e51b10a3310eb8d893586dd158a1822
USER <USER@DESKTOP-MHT6M56>
2021-01-18 13:59:12 +0900

    회원가입

---
+++ src/join/JoinBean.java
@@ -0,0 +1,52 @@
+package join;
+
+public class JoinBean {
+
+	private String id;// 아이디
+	private String pass;// 비번
+	private String name;//이름
+	private int sex;//성별 1,2
+	private int age;//나이
+	private String email;//이메일
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+}
