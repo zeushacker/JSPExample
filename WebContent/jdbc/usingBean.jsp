d94a1b98e11ee9c880882d984bae3c72a4ae650a
USER <USER@DESKTOP-MHT6M56>
2021-01-19 11:04:33 +0900

    Beans 를 이용한 디비연동

---
+++ WebContent/jdbc/usingBean.jsp
@@ -0,0 +1,57 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+<%@ page import="java.util.*, jdbc.*" %>    
+    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title> Beans를 이용한 데이터베이스 연결 </title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body  bgcolor="#ffffcc">
+<h2>Beans 를 사용한 데이터베이스 연동 </h2>
+<br><br>
+<h3>회원정보</h3>
+<table bordercolor="#0000ff" border="1">
+<tr>
+<td><strong>ID</strong></td>
+<td><strong>PASSWD</strong></td>
+<td><strong>NAME</strong></td>
+<td><strong>MEM_NUM1</strong></td>
+<td><strong>MEM_NUM2</strong></td>
+<td><strong>E_MAIL</strong></td>
+<td><strong>PHONE</strong></td>
+<td><strong>ZIPCODE/ADDRESS</strong></td>
+<td><strong>JOB</strong></td>
+</tr>
+
+<jsp:useBean id="dao" class="jdbc.tempMemberDAO" scope="page" />
+
+<%
+      Vector<tempMemberVO> vlist = dao.getMemberList();
+     
+       int counter = vlist.size();
+       
+       for(int i =0; i <vlist.size(); i++) {
+    	   
+           tempMemberVO vo = vlist.elementAt(i);
+           %>
+        <tr>
+                   <td><%=vo.getId() %></td>
+                   <td><%=vo.getPasswd() %></td>
+                   <td><%=vo.getName() %></td>
+                   <td><%=vo.getMem_num1() %></td>
+                   <td><%=vo.getMem_num2() %></td>
+                   <td><%=vo.getEmail() %></td>
+                   <td><%=vo.getPhone() %></td>
+                   <td><%=vo.getZipcode() %>/<%=vo.getAddress() %></td>
+                  <td><%=vo.getJob() %></td>
+              <%} %>
+        </tr>
+
+</table><br><br>
+total records : <%=counter %>
+</body>
+</html>
---
+++ src/jdbc/tempMemberDAO.java
@@ -0,0 +1,70 @@
+package jdbc;
+
+import java.sql.*;
+import java.util.*;
+import jdbc.tempMemberVO;
+
+public class tempMemberDAO {
+
+	private final String JDBC_DRIVER="oracle.jdbc.driver.OracleDriver";
+	private final String JDBC_URL="jdbc:oracle:thin:@localhost:1521:orcl";
+	private final String USER="scott";
+	private final String PASS ="tiger";
+	
+	public tempMemberDAO() {
+	
+		try {
+			Class.forName(JDBC_DRIVER);
+		}catch(Exception e) {
+			System.out.println("Error :Jdbc 드라이버 로딩 실패 !");
+		}
+	}
+	
+	public Vector<tempMemberVO> getMemberList() {
+		
+		Vector<tempMemberVO> vecList = new Vector<tempMemberVO>();
+		
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			conn= DriverManager.getConnection(JDBC_URL, USER, PASS);
+			
+			String strQuery ="select * from tempmember";
+			
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(strQuery);
+			
+			while(rs.next()) {
+				
+				// vo 객체 생성
+				tempMemberVO vo = new tempMemberVO();
+				// 객체 데이터베이스에 저장된 값을 가져다 저장
+				vo.setId(rs.getString("id"));
+				vo.setPasswd(rs.getString("passwd"));
+				vo.setName(rs.getString("name"));
+				vo.setMem_num1(rs.getString("mem_num1"));
+				vo.setMem_num2(rs.getString("mem_num2"));
+				vo.setEmail(rs.getString("e_mail"));
+				vo.setPhone(rs.getString("phone"));
+				vo.setZipcode(rs.getString("zipcode"));
+				vo.setAddress(rs.getString("address"));
+				vo.setJob(rs.getString("job"));
+				
+				// 벡터에 추가
+				vecList.add(vo);
+	
+			}
+		}catch(Exception ee) {
+			System.out.println("Exception :"+ee);
+		}finally {
+			
+			if(rs != null) try {rs.close();}catch(SQLException e) {}
+			if(stmt != null) try {stmt.close();}catch(SQLException e) {}
+			if(conn != null) try {conn.close();}catch(SQLException e) {}
+		}
+		return vecList;
+	}
+}
